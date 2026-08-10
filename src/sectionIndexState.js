70577a7d2fce3ad45288ace2d65530cb5fc3ebfb
Marcelo <marcelo.g.gonzalez95@gmail.com>
2026-08-09 21:08:25 -0300

    se agrega indice

---
+++ src/sectionIndexState.js
@@ -0,0 +1,6 @@
+export const HOME_SECTION_INDEX_ITEMS = [
+  { id: "buscar", label: "Buscar" },
+  { id: "librerias", label: "Librerías" },
+  { id: "clubes", label: "Clubes" },
+  { id: "novedades", label: "Novedades" },
+];
